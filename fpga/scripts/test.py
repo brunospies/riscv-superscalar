d03072a4fe9c9be5d9730fc9ef83ee61e3e58c7d
unknown <bruno.spies@ecomp.ufsm.br>
2026-05-11 13:41:19 +0200

    chore: manually move files to new directory structure to preserve ID stage logic

---
+++ fpga/scripts/test.py
@@ -0,0 +1,126 @@
+import serial
+import struct
+import time
+import os
+
+# ================= Configs =================
+PORT = 'COM11'  
+BAUD_RATE = 115200
+
+INSTRUCTIONS_FILE = '../../software/Bubble_11_elements/BubbleSort_code.txt'
+DATA_FILE = '../../software/Bubble_11_elements/BubbleSort_data.txt'
+# =================================================
+
+def parse_and_send(file_path, serial_connection):
+    """ Read file txt, get the address and data, send via UARt with the bootloader protocol (ACK) """
+
+    if not os.path.exists(file_path):
+        print(f"[WARNING] File '{file_path}' not found...")
+        return True
+
+    print(f"\n--- Processing File: {file_path} ---")
+    
+    with open(file_path, 'r') as f:
+        lines = f.readlines()
+
+    sent_lines = 0
+
+    for line_number, line in enumerate(lines, 1):
+        line = line.strip()
+        
+        # Ignore empty lines and header
+        if not line or not line.startswith("0x"):
+            continue
+
+        parts = line.split()
+        address_str = parts[0]
+        data_str = parts[1]
+
+        try:
+            address_int = int(address_str, 16)
+            data_int = int(data_str, 16)
+            
+            # STEP 1: Pack for 32 bits binary (Little-Endian)
+            address_pack = struct.pack('<I', address_int)
+            address_data = struct.pack('<I', data_int)
+            
+            # STEP 2: Send to FPGA
+            serial_connection.write(address_pack)
+            serial_connection.write(address_data)
+            serial_connection.flush()
+            
+            # STEP 3: Wait for ACK ('0x01')
+            ack = serial_connection.read(1)
+            
+            if ack == b'\x01':
+                print(f"[OK] Address: {address_str} | Written: {data_str}")
+                sent_lines += 1
+            elif ack == b'':
+                print(f"[ERROR] Timeout at address {address_str}! The FPGA didn't respond.")
+                return False
+            else:
+                print(f"[ERROR] Unexpected ACK ({ack}) at address {address_str}.")
+                return False
+
+        except ValueError:
+            print(f"[FORMAT ERROR] Error in the line {line_number}: '{line}'")
+            continue
+
+    print(f"--- Completed: {sent_lines} words stored from {file_path} ---")
+    return True
+
+# ================= Main Execution =================
+try:
+    print(f"Opening connection in {PORT} with {BAUD_RATE} bps...")
+    
+    with serial.Serial(PORT, BAUD_RATE, timeout=3) as ser:
+        time.sleep(1) # Pause for stabilize the port
+        
+        # 1. UPLOAD (Write in FPGA)
+        success_inst = parse_and_send(INSTRUCTIONS_FILE, ser)
+        success_dados = parse_and_send(DATA_FILE, ser)
+        
+        # # 2. MONITORING / DUMP OF MEMORY (Read from FPGA)
+        if success_inst and success_dados:
+
+            # Warns that the FPGA finished (Address 0xFFFFFFFF and Data 0x00000000)
+            ser.write(struct.pack('<I', 0xFFFFFFFF))
+            ser.write(struct.pack('<I', 0x00000000)) 
+            ser.flush()
+
+            final_ack = ser.read(1)
+            print(f"End command sent. ACK received: {final_ack.hex()}")
+
+            print("\n" + "="*65)
+            print("UPLOAD COMPLETED WITH SUCCESS!")
+            print("Entering in DUMP OF MEMORY MODE (Press Ctrl+C to exit)")
+            print("Start the signal 'mem_scan' in FPGA now...")
+            print("="*65 + "\n")
+            
+            # Clean any "trash" that stay in buffer
+            ser.reset_input_buffer()
+            
+            # Start address 0x10010000
+            current_address = 0x10010000
+            
+            while True:
+                # Read 4 in 4 bytes (1 word of 32 bits)
+                if ser.in_waiting >= 4:
+                    
+                    # Read 4 bytes in the buffer
+                    bytes_pack = ser.read(4)
+                    
+                    # Little-Endian ('<') for Integer Unsigned 32-bits ('I')
+                    data_32bits = struct.unpack('<I', bytes_pack)[0]
+                    
+                    print(f"Address 0x{current_address:08X} | Hex: 0x{data_32bits:08X} | Dec: {data_32bits}")
+                    
+                    # Next address
+                    current_address += 4
+                    
+                time.sleep(0.001)
+
+except serial.SerialException as e:
+    print(f"\n[FATAL ERROR] The serial port could not be opened: {e}")
+except KeyboardInterrupt:
+    print("\n\n[WARNING] Connection closed by user.")
---
+++ rtl/core/BranchDetection_unit.vhd
@@ -0,0 +1,43 @@
+-------------------------------------------------------------------------
+-- Design unit: Branch Detection Unit
+-- Description: Detect branch and generates a bubble.
+-------------------------------------------------------------------------
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all; 
+use work.RISCV_package.all;
+
+
+entity BranchDetection_unit is
+    port (  
+        instruction        : in  Instruction_type;
+        Data1_ID           : in  std_logic_vector(31 downto 0);
+        Data2_ID           : in  std_logic_vector(31 downto 0);
+        branch_decision    : out std_logic;
+        bubble_branch_ID   : out std_logic
+    );
+end BranchDetection_unit;
+
+architecture arch1 of BranchDetection_unit is
+    signal branch_prediction, branch_decision_sig : std_logic;
+
+begin
+
+    branch_prediction <= '0'; -- move to input when implement branch predictor
+
+    branch_decision_sig <= '1' when instruction = JAL or 
+                                (instruction = BEQ  and Data1_ID = Data2_ID) or
+                                (instruction = BNE  and Data1_ID /= Data2_ID) or
+                                (instruction = BLT  and signed(Data1_ID) < signed(Data2_ID)) or
+                                (instruction = BGE  and signed(Data1_ID) >= signed(Data2_ID)) or
+                                (instruction = BLTU and Data1_ID < Data2_ID) or
+                                (instruction = BGEU and Data1_ID >= Data2_ID) else
+                       '0';
+    
+    bubble_branch_ID <= '1' when branch_decision_sig /= branch_prediction else
+                        '0';
+                        
+    branch_decision <= branch_decision_sig;
+            
+end arch1;
